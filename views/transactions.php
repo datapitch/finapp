34496f5a90b493e4d803be52e91d9af0c4aa95c2
Ben Adenle <datapitch@gmail.com>
2022-06-19 21:13:54 +0100

    initial commit

---
+++ views/transactions.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Datapitch Transaction Tracking App - v1.0</title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1>Datapitch Transaction Tracking App - v1.0</h1>
+    </header>
+    <main>
+        <table>
+            <thead>
+                <th>Date</th>
+                <th>Check No.</th>
+                <th>Description</th>
+                <th>Amount</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <th></th>
+                <th></th>
+            </tfoot>
+        </table>
+    </main>
+</body>
+
+</html>
